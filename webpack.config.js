8bc50d30492888d608d626aa2de92dfa76d435b0
felixand <felix.andrean17@gmail.com>
2019-11-21 23:00:30 +0700

    remove unused function

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 var path = require("path");
-var ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
@@ -12,17 +11,11 @@ module.exports = {
     rules: [
       {
         test: /\.js$/,
-        exclude: /(node_modules|bower_components)/,
+        exclude: /(node_modules)/,
         use: {
           loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env", "@babel/preset-react"]
-          }
         }
       }
     ]
   },
-  externals: {
-    react: "commonjs react"
-  }
 };
