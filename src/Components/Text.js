a7dbeec719370316cf93720c476eb388f3f72435
Felix Andrean (ID) <felix.andrean01@Felix-andrean.local>
2020-01-28 17:14:07 +0700

    component texT

--- src/Components/Text.js
+++ src/Components/Text.js
@@ -3,38 +3,49 @@ import styled from "@emotion/styled";
 
 const H1 = styled.p`
   font-size: 30px;
+  line-height: 1.5;
 `;
 const H2 = styled.p`
   font-size: 25px;
+  line-height: 1.5;
 `;
 const H3 = styled.p`
   font-size: 20px;
+  line-height: 1.5;
 `;
 const H4 = styled.p`
   font-size: 18px;
+  line-height: 1.3;
 `;
 const H5 = styled.p`
   font-size: 16px;
+  line-height: 1.3;
 `;
 const H6 = styled.p`
   font-size: 14px;
+  line-height: 1.3;
+`;
+const P = styled.p`
+  line-height: 1.3;
 `;
 
-export const Text = props => {
+const Text = props => {
   switch (props.textType) {
     case "h1":
-      return <H1>{props.children} </H1>;
+      return <H1 style={props.styleConfig}>{props.children} </H1>;
     case "h2":
-      return <H2>{props.children} </H2>;
+      return <H2 style={props.styleConfig}>{props.children} </H2>;
     case "h3":
-      return <H3>{props.children} </H3>;
+      return <H3 style={props.styleConfig}>{props.children} </H3>;
     case "h4":
-      return <H4>{props.children}</H4>;
+      return <H4 style={props.styleConfig}>{props.children}</H4>;
     case "h5":
-      return <H5>{props.children}</H5>;
+      return <H5 style={props.styleConfig}>{props.children}</H5>;
     case "h6":
-      return <H6>{props.children}</H6>;
+      return <H6 style={props.styleConfig}>{props.children}</H6>;
     default:
-      return <p>{props.children} </p>;
+      return <P style={props.styleConfig}>{props.children}</P>;
   }
 };
+
+export { Text };
