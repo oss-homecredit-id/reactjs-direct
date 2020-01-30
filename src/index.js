3a7eddce23647319b212662bea9b797ba0176c55
Felix Andrean (ID) <felix.andrean01@Felix-andrean.local>
2020-01-30 22:09:15 +0700

    update index

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
-import { Checkbox } from "./Components/Checkbox";
-import { Button } from "./Components/Button";
-import { Input } from "./Components/Input";
-import { Text } from "./Components/Text";
+// import { Checkbox } from "./Components/Checkbox";
+// import { Button } from "./Components/Button";
+// import { Input } from "./Components/Input";
+// import { Text } from "./Components/Text";
+// import { colors } from "./assets/mixins";
 
-export { Checkbox, Button, Text, Input };
+// export { Checkbox, Button, Text, Input };
+
+export * from "./Components/Checkbox";
+export * from "./Components/Button";
+export * from "./Components/Text";
+export * from "./Components/Input";
+export * from "./Components/Select";
+export * from "./Components/Datepicker";
+export * from "./assets/mixins/mixins";
