1689b2948d01f7ba70640fe8e66a6037e7d13e7d
Felix Andrean (ID) <felix.andrean01@Felix-andrean.local>
2020-01-30 22:05:05 +0700

    add datepicker component

---
+++ src/Components/Datepicker.js
@@ -0,0 +1,26 @@
+import React from "react";
+import ReactDatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { Input } from "./Input";
+
+export const Datepicker = ({ dateFormat, selected, onChange, isDisabled }) => {
+  const ref = React.createRef();
+
+  return (
+    <>
+      <ReactDatePicker
+        selected={selected}
+        dateFormat={dateFormat ? dateFormat : "dd/MM/yyyy"}
+        onChange={date => onChange(date)}
+        customInput={
+          <Input
+            label="Date From"
+            isDisabled={isDisabled ? true : false}
+            type="date"
+            ref={ref}
+          />
+        }
+      />
+    </>
+  );
+};
